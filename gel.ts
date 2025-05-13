a9e1b732a2535594a9fb1fea33937c6ff8238717
Scott Trinh <scott@scotttrinh.com>
2025-05-13 13:49:34 -0400

    Export the drizzle and gel query builders

--- gel.ts
+++ gel.ts
@@ -1,3 +1,19 @@
+import { drizzle } from "drizzle-orm/gel";
 import { createClient } from "gel";
+import * as schema from "@/drizzle/schema";
+import * as relations from "@/drizzle/relations";
+export { default as e } from "@/dbschema/edgeql-js";
 
 export const client = createClient();
+
+export const db = drizzle({
+  client,
+  schema: {
+    ...schema,
+    ...relations,
+  },
+});
+
+export interface BaseObject {
+  id: string;
+}
